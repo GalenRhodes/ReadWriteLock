4440223977e77305be0e7b2237ed0b03f386335b
Galen Rhodes <galenrhodes@me.com>
2022-01-03 16:00:40 -0500

    Initial Commit

---
+++ ReadWriteLock/ReadWriteLock.h
@@ -0,0 +1,18 @@
+//
+//  ReadWriteLock.h
+//  ReadWriteLock
+//
+//  Created by Galen Rhodes on 1/3/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReadWriteLock.
+FOUNDATION_EXPORT double ReadWriteLockVersionNumber;
+
+//! Project version string for ReadWriteLock.
+FOUNDATION_EXPORT const unsigned char ReadWriteLockVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReadWriteLock/PublicHeader.h>
+
+
